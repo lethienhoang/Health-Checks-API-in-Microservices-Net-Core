6e8aa37c478171f39668ea0e7c99e84e1ff90757
lethienhoang <45428011+lethienhoang@users.noreply.github.com>
2020-01-04 19:38:38 +0700

    init app

---
+++ HealthChecks_API/HealthChecks_API/SqlConnectionHealthCheck.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthChecks_API
+{
+    public class SqlConnectionHealthCheck : IHealthCheck
+    {
+        private readonly string _defaultSqlQuery = "Select 1";
+        private string _connectionString { get; }
+
+        public SqlConnectionHealthCheck(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+
+                if (_defaultSqlQuery != null)
+                {
+                    var command = connection.CreateCommand();
+                    command.CommandText = _defaultSqlQuery;
+
+                    await command.ExecuteNonQueryAsync(cancellationToken);
+                }
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
---
+++ HealthChecks_API/HealthChecks_API/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HealthChecks_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddHealthChecks()
+                .AddCheck("Database health check", new SqlConnectionHealthCheck(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddHealthChecksUI();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            app.UseHealthChecksUI(options =>
+            {
+                options.UIPath = "/health-ui";
+            });
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
